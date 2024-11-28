dec894aa77259fb8d6ef0e0706abc3d8dbcfc578
Miguel Alejandro <migtorruco@gmail.com>
2024-11-28 14:46:10 -0600

    feat: ui of the login and register forms

--- src/components/registration/Login.jsx
+++ src/components/registration/Login.jsx
@@ -1,28 +1,67 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 /* 
 A form to allow a user to login
 */
 export default function Login() {
-    return (
-        <Form >
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Correo electrónico</Form.Label>
-                <Form.Control required type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                   Nunca compartiremos tu correo con nadie
-                </Form.Text>
-            </Form.Group>
+    const loginEndpoint = "http://localhost:3000/";
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Contraseña</Form.Label>
-                <Form.Control required type="password" placeholder="Password" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Enviar
-            </Button>
-        </Form>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <Container
+            fluid
+            className="d-flex justify-content-center align-items-center vh-100"
+        >
+            <Row>
+                <Col>
+                    <Form
+                        onSubmit={handleSubmit}
+                        className="p-4 shadow rounded bg-white login-form"
+                    >
+                        <h3 className="text-center mb-3">Inicia sesión</h3>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Nombre de usuario</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="Ingresa tu username"
+                            />
+                        </Form.Group>
+
+                        <Form.Group
+                            className="mb-3"
+                            controlId="formBasicPassword"
+                        >
+                            <Form.Label>Contraseña</Form.Label>
+                            <Form.Control
+                                required
+                                type="password"
+                                placeholder="Contraseña"
+                            />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3">
+                            <Form.Text>
+                                ¿No tienes una cuenta?{" "}
+                                <Link to="/register">Regístrate</Link>
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Button
+                            variant="primary"
+                            type="submit"
+                            className="w-100"
+                        >
+                            Enviar
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     );
 }
--- src/components/registration/Register.jsx
+++ src/components/registration/Register.jsx
@@ -1,9 +1,55 @@
 import React from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
 
 export default function Register() {
+    const registerEndpoint = "http://localhost:3000/register";
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <div>
-            <h3>Haz tu cuenta</h3>
-        </div>
+        <Container
+            fluid
+            className="d-flex justify-content-center align-items-center vh-100"
+        >
+            <Row>
+                <Col>
+                    <Form
+                        onSubmit={handleSubmit}
+                        className="p-4 shadow rounded bg-white login-form"
+                    >
+                        <h3 className="text-center mb-3">Regístrate</h3>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Nombre de usuario</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="Ingresa tu username"
+                            />
+                        </Form.Group>
+
+                        <Form.Group
+                            className="mb-3"
+                            controlId="formBasicPassword"
+                        >
+                            <Form.Label>Contraseña</Form.Label>
+                            <Form.Control
+                                required
+                                type="password"
+                                placeholder="Contraseña"
+                            />
+                        </Form.Group>
+                        <Button
+                            variant="primary"
+                            type="submit"
+                            className="w-100"
+                        >
+                            Enviar
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     );
 }
